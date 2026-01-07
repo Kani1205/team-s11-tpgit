31f35ad62a040f918f87ccfc7694983a461535a7
Kani1205 <kanimozhin120@gmail.com>
2026-01-07 03:14:46 -0800

    Create problem8.py

---
+++ problem8.py
@@ -0,0 +1,13 @@
+student []
+while true:
+try:
+name,department=input().split()
+student.append((name.department))
+except:
+student sort(key=lambda x:x[1])
+for i,s in enumerate(student):
+print(f"seat{i+1}-{s[0]}({s[0]}")
+    
+
+
+ 
